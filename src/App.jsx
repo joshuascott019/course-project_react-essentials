02d6d1f830b2528a01dcfb358ae253eb2846dddb
Joshua Scott <joshua.ds.s019@gmail.com>
2024-02-23 11:54:34 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  let tabContent = <p>Please select a topic.</p>;
+
+  function handleClick(clickedButton) {
+    setSelectedTopic(clickedButton);
+  }
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((item) => (
+              <CoreConcepts key={item.title} {...item} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === 'components'}
+              onClick={() => handleClick('components')}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onClick={() => handleClick('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onClick={() => handleClick('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onClick={() => handleClick('state')}
+            >
+              State
+            </TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
